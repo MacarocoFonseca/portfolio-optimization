ef6c6878e1b946e2161869bbb59075d840a982af
Tomás Zúquete da Fonseca <t.zuquete.f@gmail.com>
2024-01-04 00:05:47 +0100

    Add .gitignore file

--- src_code/.ipynb_checkpoints/lump_sum_simulator-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import yfinance as yf
-
-
-class LumpSumInvestmentStrategy:
-    """
-    """
-    def __init__(self, ticker, start_date, end_date):
